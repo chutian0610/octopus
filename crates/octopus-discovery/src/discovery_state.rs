ce374e28398690ebf757c23de6d4eced23779d13
chutian0610 <victorchu0610@outlook.com>
2025-03-03 21:15:57 +0800

    rust

---
+++ crates/octopus-discovery/src/discovery_state.rs
@@ -0,0 +1,6 @@
+trait LocalDiscoveryStore {
+    
+}
+trait RemoteDiscoveryStore {
+    
+}
--- crates/octopus-discovery/src/lib.rs
+++ crates/octopus-discovery/src/lib.rs
@@ -1,2 +1,24 @@
+///           ┌─────────────┐     
+///           │             │     
+///    ┌──────┼  Discovery  │     
+///    │      │    Server   │     
+///    │      │             │     
+///    │      └────────▲────┘     
+///    │               │          
+///    │LookUp         │Announce  
+///    │               │UnAnnounce
+///    │               │          
+/// ┌──▼──────────┐    │          
+/// │             │    │          
+/// │  Discovery  ┼────┘          
+/// │    Client   │               
+/// │             │               
+/// └─────────────┘               
+
 pub mod discovery_server;
 pub mod discovery_client;
+mod discovery_state;
+
+pub struct ServiceInstance {
+
+}
