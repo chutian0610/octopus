38fb94f9430cc288c25ad24037e5cbed8aebb234
chutian0610 <victorchu0610@outlook.com>
2025-05-28 11:32:19 +0800

    Time utils

--- crates/octopus-common/src/time_util.rs
+++ crates/octopus-common/src/time_util.rs
@@ -1,12 +1,11 @@
 use chrono::prelude::*;
-use chrono_tz::{ParseError, Tz};
+use chrono_tz::Tz;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use chrono::DateTime;
 
 pub static FORMAT_PATTERN_DATETIME: &str = "%Y-%m-%d %H:%M:%S";
 pub static FORMAT_PATTERN_DATETIME_WITH_MICRO: &str = "%Y-%m-%d %H:%M:%S%.3f";
-
 pub fn to_millis(time: SystemTime) -> u64 {
     time.duration_since(UNIX_EPOCH).unwrap().as_millis() as u64
 }
@@ -32,53 +31,21 @@ pub fn from_nanos(ts: u128) -> SystemTime {
         + Duration::from_nanos((ts % 1_000_000_000) as u64)
 }
 
-pub fn format_local_time(st: SystemTime) -> String {
+pub fn print_local_time(st: SystemTime) -> String {
     let local_datetime: DateTime<Local> = st.into();
     local_datetime.format(FORMAT_PATTERN_DATETIME).to_string()
 }
 
-pub fn format_local_time_with_pattern(st: SystemTime, pattern: &str) -> String {
+pub fn print_local_time_with_pattern(st: SystemTime, pattern: &str) -> String {
     let local_datetime: DateTime<Local> = st.into();
     local_datetime.format(pattern).to_string()
 }
 
-pub fn parse_local_time(datetime_str: &str, time_zone_str: &str) -> Result<SystemTime, String> {
-    let tz: Tz = time_zone_str
-        .parse()
-        .map_err(|e: ParseError| format!("parsing time zone string failed: {}", e))?;
-    let naive_datetime = NaiveDateTime::parse_from_str(datetime_str, FORMAT_PATTERN_DATETIME)
-        .map_err(|e| format!("parsing date string failed: {}", e))?;
-    let local_datetime = Local
-        .from_local_datetime(&naive_datetime)
-        .single()
-        .map(|time| time.with_timezone(&tz))
-        .ok_or_else(|| "cannot be converted to local time".to_string())?;
-    Ok(local_datetime.into())
-}
-
-pub fn parse_local_time_with_pattern(
-    datetime_str: &str,
-    time_zone_str: &str,
-    pattern: &str,
-) -> Result<SystemTime, String> {
-    let tz: Tz = time_zone_str
-        .parse()
-        .map_err(|e: ParseError| format!("parsing time zone string failed: {}", e))?;
-    let naive_datetime = NaiveDateTime::parse_from_str(datetime_str, pattern)
-        .map_err(|e| format!("parsing date string failed: {}", e))?;
-    let local_datetime = Local
-        .from_local_datetime(&naive_datetime)
-        .single()
-        .map(|time| time.with_timezone(&tz))
-        .ok_or_else(|| "cannot be converted to local time".to_string())?;
-    Ok(local_datetime.into())
-}
-
-pub fn format_utc_time(st: SystemTime) -> String {
+pub fn print_utc_time(st: SystemTime) -> String {
     let utc_datetime: DateTime<Utc> = st.into();
     utc_datetime.format(FORMAT_PATTERN_DATETIME).to_string()
 }
-pub fn format_utc_time_with_pattern(st: SystemTime, pattern: &str) -> String {
+pub fn print_utc_time_with_pattern(st: SystemTime, pattern: &str) -> String {
     let utc_datetime: DateTime<Utc> = st.into();
     utc_datetime.format(pattern).to_string()
 }
@@ -100,6 +67,24 @@ pub fn parse_utc_time_with_pattern(
     Ok(utc_datetime.into())
 }
 
+pub fn custom_timezone(
+    time_str: &str,
+    format: &str,
+    src_tz: &str,
+    target_tz: &str,
+) -> Result<DateTime<Tz>, String> {
+    let src_timezone = src_tz
+        .parse::<Tz>()
+        .map_err(|e: chrono_tz::ParseError| format!("解析src_tz失败: {}", e))?;
+    let naive = chrono::NaiveDateTime::parse_from_str(time_str, format)
+        .map_err(|e: chrono::ParseError| format!("解析日期字符串失败: {}", e))?;
+    let src_time = src_timezone.from_local_datetime(&naive).unwrap();
+    let target_timezone = target_tz
+        .parse::<Tz>()
+        .map_err(|e: chrono_tz::ParseError| format!("解析target_tz失败: {}", e))?;
+    Ok(src_time.with_timezone(&target_timezone))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -109,6 +94,21 @@ mod tests {
         let time = parse_utc_time("2025-04-03 07:52:39");
         assert!(time.is_ok());
         assert_eq!(to_millis(time.unwrap()), 1743666759000);
+    }
+    #[test]
+    fn test_from_millis() {
+        let time = from_millis(1743666759000);
+        assert_eq!(to_millis(time), 1743666759000);
+    }
+    #[test]
+    fn test_format_local_time() {
+        let time = from_millis(1743666759000);
+        assert_eq!(print_local_time(time), "2025-04-03 15:52:39");
+    }
+    #[test]
+    fn test_format_utc_time() {
+        let time = from_millis(1743666759000);
+        assert_eq!(print_utc_time(time), "2025-04-03 07:52:39");
     }
 
     #[test]
@@ -121,17 +121,5 @@ mod tests {
         let time = parse_utc_time("2025-04-03 07:52:39");
         assert!(time.is_ok());
         assert_eq!(time.unwrap(), local_time);
-    }
-    #[test]
-    fn test_parse_local_time() {
-        let tz: Tz = "Asia/Shanghai".parse().unwrap();
-        let local_time = Utc
-            .with_ymd_and_hms(2025, 4, 3, 7, 52, 39)
-            .single()
-            .unwrap()
-            .with_timezone(&tz);
-        let time = parse_local_time("2025-04-03 07:52:39", "Asia/Shanghai");
-        let time2: SystemTime = local_time.into();
-        assert_eq!(time.unwrap(), time2);
     }
 }
