c57372be01dad10345bfe24f08f0b7f10fd41923
chutian0610 <victorchu0610@outlook.com>
2026-05-11 10:48:50 +0000

    feat(03-02): add Arrow Flight protobuf definition and dependencies
    - Add proto/worker.proto with WorkerFlightService and FlightData messages
    - Add proto/build.rs for tonic protobuf code generation
    - Enable arrow-flight 53 and arrow 53 dependencies in octopus-worker

---
+++ proto/build.rs
@@ -0,0 +1,9 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(true)
+        .build_transport(true)
+        .compile_protos(&["proto/worker.proto"], &["proto"])?;
+
+    Ok(())
+}
